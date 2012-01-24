142256b278dc6679158ceda5248b7673989baf69
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-01-24 20:40:45 -0200

    Contém protótipo de classe também

---
+++ esqueleto1-portlet/WEB-INF/src/br/com/seatecnologia/blog/portlet/NOME DA CLASSE DE PORTLET.java
@@ -0,0 +1,14 @@
+package br.com.seatecnologia.blog.portlet;
+
+import javax.portlet.GenericPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+public class {NOME DA CLASSE DE PORTLET} extends GenericPortlet {
+
+}
+
